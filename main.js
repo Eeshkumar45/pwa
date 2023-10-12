fb0beb1d69ab0a391f01e2d0ea91c4940022aa75
Eeshkumar Siddenthi <eeshkumar.s@Technovert.com>
2023-10-13 00:24:52 +0530

    notification-sending-pwa

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+console.log("main.js");
+addEventListener("load", (event) => {Notification.requestPermission()});
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
+}
+
+
+function sendMessage(){
+    navigator.serviceWorker.ready.then((registration) => {
+        registration.active.postMessage(
+          document.getElementById("msg").value
+        );
+      });
 }
--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
 console.log("default")
+sendNotification("default")
 
 addEventListener("install", (event)=>{
     console.log("install");
+    sendNotification("install")
 })
+
+addEventListener("activate", (event)=>{
+    console.log("activate");
+    sendNotification("activate")
+})
+
+addEventListener("message", (event)=>{
+    console.log("message");
+    sendNotification(event.data)
+})
+
+function sendNotification(title){
+    self.registration.showNotification(title)
+}
