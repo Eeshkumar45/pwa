543b4e64f3ea918434bb7b50812b1aca75927213
Eeshkumar Siddenthi <eeshkumar.s@Technovert.com>
2023-10-13 01:43:59 +0530

    background-notifications-loop-pwa

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
 console.log("main.js");
-addEventListener("load", (event) => {Notification.requestPermission()});
+addEventListener("load", (event) => {
+  Notification.requestPermission();
+});
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+
+function startPeriodicSync() {
+    if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.ready.then(function(reg) {
+            if (reg.periodicSync) {
+                reg.periodicSync.register({
+                        tag: 'periodicSync',
+                        minPeriod: 0,
+                        powerState: 'auto',
+                        networkState: 'any'
+                    })
+                    .then(function(event) {
+                        console.log('Periodicc registration successful', event);
+                    })
+                    .catch(function(error) {
+                        console.log('Periodicc registration failed', error);
+                    });
+            } else {
+                console.log("Background not supported");
+            }
+        });
+    } else {
+        console.log("Nove ServiceWorker");
+    }
 }
 
 
-function sendMessage(){
-    navigator.serviceWorker.ready.then((registration) => {
-        registration.active.postMessage(
-          document.getElementById("msg").value
-        );
-      });
+function sendMessage() {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.active.postMessage(document.getElementById("msg").value);
+  });
+}
+
+function startSync() {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.sync.register("startNotificationSync");
+  });
 }
--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-console.log("default")
-sendNotification("default")
-count = 0
-addEventListener("install", (event)=>{
-    console.log("install");
-    setInterval(()=>sendNotification("install : "+(++count)),3000);
-})
+console.log("default");
+sendNotification("default");
+installNotificationCount = 0;
+syncNotificationCount = 0;
+addEventListener("install", (event) => {
+  console.log("install");
+  setInterval(
+    () => sendNotification("install : " + ++installNotificationCount),
+    3000
+  );
+});
 
-addEventListener("activate", (event)=>{
-    console.log("activate");
-    sendNotification("activate")
-})
+addEventListener("activate", (event) => {
+  console.log("activate");
+  sendNotification("activate");
+});
 
-addEventListener("message", (event)=>{
-    console.log("message");
-    sendNotification(event.data)
-})
+addEventListener("message", (event) => {
+  console.log("message");
+  sendNotification(event.data);
+});
 
-function sendNotification(title){
-    self.registration.showNotification(title)
+self.addEventListener("sync", (event) => {
+  if (event.tag === "startNotificationSync") {
+    event.waitUntil(
+      setInterval(
+        () => sendNotification("sync : " + ++syncNotificationCount),
+        3000
+      )
+    );
+  }
+});
+
+self.addEventListener('periodicsync', function(event) {
+    console.log(event)
+    if (event.registration.tag == "periodicSync") {
+        console.log("Periodic event occurred: ", event);
+    }
+});
+
+function sendNotification(title) {
+  self.registration.showNotification(title);
 }
